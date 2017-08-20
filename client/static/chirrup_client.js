798c28f88043517d1b370df40815c63745948455
jennitormala <tormalajenni@gmail.com>
2017-08-20 16:43:52 +0300

    add showing data from api

--- client/static/chirrup_client.js
+++ client/static/chirrup_client.js
@@ -1,37 +1,56 @@
-
-
 const ENTRYPOINT = "http://localhost:5000/rooms/";
 var DEBUG = true;
 
-/** 
- * Mason+JSON mime-type 
+/**
+ * Mason+JSON mime-type
  * @constant {string}
- * @default 
+ * @default
  */
 const MASONJSON = "application/vnd.mason+json";
 
 const PLAINJSON = "application/json";
 const DEFAULT_DATATYPE = "json";
 
-
-
-
-
-
 function get_room(apiurl){
     apiurl = apiurl || ENTRYPOINT;
     $.ajax({
         url: apiurl,
         dataType:DEFAULT_DATATYPE
+    }).always(function(){
+
+        $("#roomlist").empty();
+
     }).done(function (data, textStatus, jqXHR){
         if (DEBUG) {
             console.log ("RECEIVED RESPONSE: data:",data,"; textStatus:",textStatus);
         }
         var room_url = data["@controls"].self.href;
         var admin = data.admin;
-        var name =  data.name;
-        appendRoomToList(room_url, admin, name);
-        console.log("KMDSKFMSDKFMSDKFMSDKMFS");
+
+        for (i = 0; i < data["rooms-all"].length; i++) {
+
+            var name =  data["rooms-all"][i].name;
+            appendRoomToList(room_url, admin, name);
+        }
+
+                var create_ctrl = data["@controls"]["add-room"]                                                 
+
+                if (create_ctrl.schema) {
+                    createFormFromSchema(create_ctrl.href, create_ctrl.schema, "new_room_form");
+                }
+                else if (create_ctrl.schemaUrl) {
+                    $.ajax({
+                        url: create_ctrl.schemaUrl,
+                        dataType: DEFAULT_DATATYPE
+                    }).done(function (data, textStatus, jqXHR) {
+                        createFormFromSchema(create_ctrl.href, data, "new_room_form");
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        if (DEBUG) {
+                            console.log ("RECEIVED ERROR: textStatus:",textStatus, ";error:",errorThrown);
+                        }
+                        alert ("Could not fetch form schema.  Please, try again");
+                    });
+                }
 
     }).fail(function (jqXHR, textStatus, errorThrown){
         if (DEBUG) {
@@ -42,24 +61,17 @@ function get_room(apiurl){
 }
 
 function appendRoomToList(url, admin, name) {
-        
-    var $room = $("<div>").addClass('room').html(""+
-                        "<form action='"+url+"'>"+
-                        "   <div class='form_content'>"+
-                        "       <input type=text class='admin' value='"+admin+"' readonly='readonly'/>"+
-                        "       <div class='name'>"+name+"</div>"+
-                        "   </div>"+
-                        "   <div class='commands'>"+
-                        "        <input type='button' class='deleteButton deleteMessage' value='Delete'/>"+
-                        "   </div>" +
-                        "</form>"
-                    );
-    //Append to list
-    $("#rooms_list").append($room);
 
+
+    var $room = $('<tr><td>' + name + '</td> ' +
+        '<td><a href=' + url + ' class = "btn btn-info btn-sm"> <span class="glyphicon glyphicon-plus"></span> Join </a></td></tr>');
+    //Append to list
+    $("#roomlist").append($room);
 
 }
 
-
-
 get_room(ENTRYPOINT);
+
+
+
+
