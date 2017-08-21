bb3346bb92710843d31f4facc658192c36189182
Annukka Törmälä <tormala.annukka@gmail.com>
2017-08-21 14:47:45 +0300

    get nickname to profile page

--- client/static/chirrup_client.js
+++ client/static/chirrup_client.js
@@ -6,12 +6,12 @@ var DEBUG = true;
  * @constant {string}
  * @default
  */
-const MASONJSON = "application/vnd.mason+json";
+ const MASONJSON = "application/vnd.mason+json";
 
-const PLAINJSON = "application/json";
-const DEFAULT_DATATYPE = "json";
+ const PLAINJSON = "application/json";
+ const DEFAULT_DATATYPE = "json";
 
-function get_room(apiurl){
+ function get_room(apiurl){
     apiurl = apiurl || ENTRYPOINT;
     $.ajax({
         url: apiurl,
@@ -33,24 +33,24 @@ function get_room(apiurl){
             appendRoomToList(room_url, admin, name);
         }
 
-                var create_ctrl = data["@controls"]["add-room"]                                                 
+        var create_ctrl = data["@controls"]["add-room"]                                                 
 
-                if (create_ctrl.schema) {
-                    createFormFromSchema(create_ctrl.href, create_ctrl.schema, "new_room_form");
+        if (create_ctrl.schema) {
+            createFormFromSchema(create_ctrl.href, create_ctrl.schema, "new_room_form");
+        }
+        else if (create_ctrl.schemaUrl) {
+            $.ajax({
+                url: create_ctrl.schemaUrl,
+                dataType: DEFAULT_DATATYPE
+            }).done(function (data, textStatus, jqXHR) {
+                createFormFromSchema(create_ctrl.href, data, "new_room_form");
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                if (DEBUG) {
+                    console.log ("RECEIVED ERROR: textStatus:",textStatus, ";error:",errorThrown);
                 }
-                else if (create_ctrl.schemaUrl) {
-                    $.ajax({
-                        url: create_ctrl.schemaUrl,
-                        dataType: DEFAULT_DATATYPE
-                    }).done(function (data, textStatus, jqXHR) {
-                        createFormFromSchema(create_ctrl.href, data, "new_room_form");
-                    }).fail(function (jqXHR, textStatus, errorThrown) {
-                        if (DEBUG) {
-                            console.log ("RECEIVED ERROR: textStatus:",textStatus, ";error:",errorThrown);
-                        }
-                        alert ("Could not fetch form schema.  Please, try again");
-                    });
-                }
+                alert ("Could not fetch form schema.  Please, try again");
+            });
+        }
 
     }).fail(function (jqXHR, textStatus, errorThrown){
         if (DEBUG) {
@@ -59,6 +59,40 @@ function get_room(apiurl){
         alert("Cannot get information from message: "+ apiurl);
     });
 }
+
+
+
+function get_user(apiurl){
+    apiurl = "http://localhost:5000/users/";
+    $.ajax({
+        url: apiurl,
+        dataType:DEFAULT_DATATYPE
+    }).always(function(){
+
+        $("#nickname").empty();
+
+    }).done(function (data, textStatus, jqXHR){
+        if (DEBUG) {
+            console.log ("RECEIVED RESPONSE: data:",data,"; textStatus:",textStatus);
+        }
+        var user_url = data["@controls"].self.href;
+        var user = data.user;
+
+
+        var nickname =  data["users-all"][0].nickname;
+        appendNicknameToProfile(user_url, nickname);
+        
+
+
+    }).fail(function (jqXHR, textStatus, errorThrown){
+        if (DEBUG) {
+            console.log ("RECEIVED ERROR: textStatus:",textStatus, ";error:",errorThrown);
+        }
+        alert("Cannot get information from message: "+ apiurl);
+    });
+}
+
+
 
 function appendRoomToList(url, admin, name) {
 
@@ -70,7 +104,22 @@ function appendRoomToList(url, admin, name) {
 
 }
 
+function appendNicknameToProfile(url, nickname) {
+
+
+    var $user = $('<h4>' + 'Nickname: ' +'  '+ nickname +'</h4>');
+    //Append to list
+    $("#nickname").append($user);
+
+}
+
+
+
+
+
+
 get_room(ENTRYPOINT);
+get_user(ENTRYPOINT);
 
 
 
