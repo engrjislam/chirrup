47ebd46331d1d4c1de4a3bcfa1cea95648c25dce
jennitormala <tormalajenni@gmail.com>
2017-08-28 12:37:29 +0300

    add template folder path

--- client/client.py
+++ client/client.py
@@ -1,3 +1,3 @@
 from flask import Flask
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='static', static_url_path='', template_folder='static')
 app.debug=True
